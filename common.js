e3d71aeedb84f36764f3d1da21ec2c91ab634888
bbx <bbx@bbx.com>
2023-08-01 00:54:02 +0800

    0.4

--- common.js
+++ common.js
@@ -2,14 +2,16 @@ const Q = Node.prototype.Q = function(el, func) {
     let els = this.querySelectorAll?.(el) ?? document.querySelectorAll(el);
     return func ? els.forEach(func) : els.length > 1 ? [...els] : els[0];
 }
-const E = (el, attr) => {
+const E = (el, attr, children) => {
+    (Array.isArray(attr) || typeof attr == 'string') && (children = attr);
     el == 'img' && (attr = {alt: 'image', ...attr ?? {}});
     el = document.createElement(el);
+    el.append(...children ?? []);
     Object.assign(el.style, attr?.style ?? {});
     return Object.assign(el, (({style, ...attr}) => attr)(attr ?? {}));
 }
 E.figure = ({imgs, caption, ...attr}) =>
-    [...[imgs].flat().map(src => E('img', {src})), E('figcaption', {innerHTML: caption})]
+    [E('figcaption', {innerHTML: caption}), ...[imgs].flat().map(src => E('img', {src}))]
     .reduce((figure, child) => figure.appendChild(child).parentElement, E('figure', attr));
 E.ul = lis => [...[lis].flat()].map(li => E('li', {innerHTML: li})).reduce((ul, li) => ul.appendChild(li).parentElement, E('ul'));
 E.ol = lis => [...[lis].flat()].map(li => E('li', {innerHTML: li})).reduce((ol, li) => ol.appendChild(li).parentElement, E('ol'));
